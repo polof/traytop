3c500d1232cd3bc7619ea66272a28b6cac05d05c
Per Olofsson <pelle@dsv.su.se>
2013-08-05 12:44:49 +0200

    add COPYING file

--- extension.js
+++ extension.js
@@ -5,7 +5,7 @@
 
   This work is free. You can redistribute it and/or modify it under the
   terms of the Do What The Fuck You Want To Public License, Version 2,
-  as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+  as published by Sam Hocevar. See the COPYING file for more details.
 */
 
 const Lang = imports.lang;
